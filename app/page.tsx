70f7b2f5f9986bf5b8077b1bb51556a007384316
Rhuann01 <rhuann2006@gmail.com>
2026-04-16 21:13:47 -0300

    chor: add shadcn theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import {Button} from "./_components/ui/button"
+import { Switch } from "./_components/ui/switch"
+
 export default function Home(){
   return (
-    <section>
-      <h1 className=" flex items-center justify-center w-screen h-screen bg-neutral-800 text-white font-black ">Hello next, quanto tempo</h1>
+    <section className=" flex flex-col gap-2 items-center justify-center w-screen h-screen bg-neutral-800 text-white font-black ">
+      <h1 className="">Hello next, quanto tempo</h1>
+      <Button> Teste </Button> 
+      <Switch />
     </section>
   )
 }
