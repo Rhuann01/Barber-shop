b5e25378393bd9e7d19976989f711fd291e7327b
Rhuann01 <rhuann2006@gmail.com>
2026-04-17 21:16:13 -0300

    feat: add booking item

--- app/_components/ui/header.tsx
+++ app/_components/ui/header.tsx
@@ -5,8 +5,8 @@ import { MenuIcon } from "lucide-react";
 
 export default function Header() {
   return (
-    <Card className=" bg-neutral-950">
-      <CardContent className=" flex items-center justify-between p-5">
+    <Card>
+      <CardContent className=" flex items-center justify-between">
         <Image alt="Logo" src="/Logo.png" width={120} height={18} />
         <Button size="icon">
           <MenuIcon />
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Header from "./_components/ui/header";
 import Image from "next/image";
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { Badge } from "./_components/ui/badge";
+import { Avatar, AvatarImage } from "./_components/ui/avatar";
 
 export default function Home() {
   return (
@@ -31,12 +32,23 @@ export default function Home() {
         </div>
 
         <Card className="mt-5">
-          <CardContent>
-            <div className=" flex w-full h-full">
-              <div className="flex flex-col px-5 pl-7 gap-2">
-                <Badge>Confirmado</Badge>
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 pl-7">
+              <Badge className="bg-purple-950 text-purple-300">
+                Confirmado
+              </Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="w-6 h-6">
+                  <AvatarImage src="https://i.pinimg.com/1200x/a1/8e/5f/a18e5f09224c7688da94720f387a2c48.jpg" />
+                </Avatar>
+                <p className="font-light">Vintager Barber</p>
               </div>
-              <div></div>
+            </div>
+            <div className=" flex flex-col w-30 px-5 text-sm items-center justify-center border-l-2 border-solid">
+              <p>Abril</p>
+              <p className="text-2xl font-semibold">17</p>
+              <p>21:01</p>
             </div>
           </CardContent>
         </Card>
